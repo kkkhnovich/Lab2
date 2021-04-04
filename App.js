447a6bc097b13dae3a63172b252642467ca1d199
kkkhnovich <74446556+kkkhnovich@users.noreply.github.com>
2021-04-04 20:15:50 +0300

    Update App.js

--- App.js
+++ App.js
@@ -5,11 +5,21 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header';
 
+let element = function Image(){
+  <Navbar.Brand href="/">
+  <img
+      src={logo}
+      height="30"
+      width="30"
+      className="d-inline-block align-top"
+      alt="logo"
+  /> Hello World
+  </Navbar.Brand>
+};
+
 function App() {
   return (
-    <div>
-   <Header />
-    </div>
+     Header(element)
   );
 }
 
